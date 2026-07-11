a7183ac5aad474e9dbf300530a0964e263e21dab
Alejandro <alemancar0511@gmail.com>
2026-07-11 17:51:54 -0500

    fix: configurar despliegue en Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { StoreProvider } from './context/StoreContext';
 import { AdminPage } from './pages/AdminPage';
@@ -10,5 +10,22 @@ import { OrdersPage } from './pages/OrdersPage';
 import { ProductDetailPage } from './pages/ProductDetailPage';
 
 export default function App() {
-  return <BrowserRouter><StoreProvider><Routes><Route element={<Layout />}><Route path="/" element={<HomePage />} /><Route path="/catalogo" element={<CatalogPage />} /><Route path="/producto/:id" element={<ProductDetailPage />} /><Route path="/checkout" element={<CheckoutPage />} /><Route path="/confirmacion/:id" element={<ConfirmationPage />} /><Route path="/pedidos" element={<OrdersPage />} /><Route path="/admin" element={<AdminPage />} /><Route path="*" element={<HomePage />} /></Route></Routes></StoreProvider></BrowserRouter>;
+  return (
+    <HashRouter>
+      <StoreProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalogo" element={<CatalogPage />} />
+            <Route path="/producto/:id" element={<ProductDetailPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/confirmacion/:id" element={<ConfirmationPage />} />
+            <Route path="/pedidos" element={<OrdersPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </StoreProvider>
+    </HashRouter>
+  );
 }
